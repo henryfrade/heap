a79bf80e5fdc3e7f76884276c2c8f3f8cfba9b93
Henry Frade <henry.frade@whitehatsec.com>
2020-04-28 17:07:53 -0500

    create Add method

--- main.go
+++ main.go
@@ -17,3 +17,19 @@ func NewHeap() *heap {
 		array: make([]int, 0, 100),
 	}
 }
+
+func (h *heap) Add(i int) {
+	h.array = append(h.array, i)
+	//fmt.Println(fmt.Sprintf("Add %v: [%s]", i, h.String()))
+	curr := len(h.array) - 1
+	for curr > 0 {
+		papa := (curr - 1) / 2
+		if h.array[papa] < h.array[curr] {
+			t := h.array[papa]
+			h.array[papa] = h.array[curr]
+			h.array[curr] = t
+		}
+		curr = papa
+	}
+}
+
